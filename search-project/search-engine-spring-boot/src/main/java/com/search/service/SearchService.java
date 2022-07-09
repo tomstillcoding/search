97ed5cd60c90c1efc4f73ab586ba003c3e191b6d
tomstillcoding <2035189076@qq.com>
2022-07-09 21:17:55 +0800

    add

--- search-project/search-engine-spring-boot/src/main/java/com/search/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package com.search.service;
-
-import java.util.Map;
-
-
-public interface SearchService {
-    // 通过
-    Map<String, Object> getDataByKeyword(String keyword, int pageSize, int pageNum);
-}
